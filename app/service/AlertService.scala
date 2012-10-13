76d6296d6c8fd802f143135cb8f33792c104bad4
Choon Kee <choonkee@wego.com>
2012-10-13 21:41:57 +0800

    change polling interval to 1/2 hour

--- app/service/AlertService.scala
+++ app/service/AlertService.scala
@@ -25,20 +25,8 @@ with CheckerComponent
 with SearchComponent
 with SchedulerComponent{
 
-  //val serializer = new JavaSerializerImpl
-  //val store = new RedisStoreImpl
-  //val exchangeRateComponent = new ExchangeRateComponentImpl 
-  //val notificatorActorRef = system.actorOf(Props(new NotificatorActor).withRouter(
-      //SmallestMailboxRouter(nrOfInstances = 5)))
-  //val bestPricerComponent = system.actorOf(Props(new BestPricerActor).withRouter(
-      //SmallestMailboxRouter(nrOfInstances = 5)))
-  //val checkerActorRef = system.actorOf(Props(new CheckerActor).withRouter(
-      //SmallestMailboxRouter(nrOfInstances = 5)))
-  //val searchActorRef = system.actorOf(Props(new SearchActor).withRouter(
-      //SmallestMailboxRouter(nrOfInstances = 5)))
-  //val scheduler = system.actorOf(Props(new SchedulerActor))
 
-  //system.scheduler.schedule(0 seconds, 1 hour, scheduler, CheckMonitor)
+  system.scheduler.schedule(0 seconds, 30  minutes, scheduler, CheckMonitor)
 
   def register(
     phoneNumber:String,
@@ -48,7 +36,7 @@ with SchedulerComponent{
     price:Float){
 
       val user = User(phoneNumber, phoneNumber)
-      val searchRequest = SearchRequest(origin, destination, departureDate)
+      val searchRequest = SearchRequest(origin, destination, departureDate, returnDate=departureDate)
       val monitor = new Monitor(user.id, searchRequest, price)
       store.storeUser(user)
       store.storeMonitor(monitor)
